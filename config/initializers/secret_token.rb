7983ef7f399e0a6237479b58818283de19008896
Walkmaster <walkmasterco@gmail.com>
2014-01-10 23:22:37 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cobaltazure::Application.config.secret_key_base = '337c82c728d8bb7821c7e6c13f27f0074b9455f76ac9e1d8ad27a3a9fb83fc6ce6006cda8c3f73316f660371986df5e3863ba0c6d17df1ab5afdbae312b16ea0'
